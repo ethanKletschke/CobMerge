a9cc0a8e06e4d1bf8fb34bad9886b73dd3670fd0
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-17 13:19:43 +0200

    refactor: Rename input file identifiers

--- src/main.cbl
+++ src/main.cbl
@@ -4,15 +4,15 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT 1st-In-File ASSIGN TO "Input1.csv"
+           SELECT 1st-Input-File ASSIGN TO "Input1.csv"
            ORGANISATION LINE SEQUENTIAL
            ACCESS MODE SEQUENTIAL.
 
-           SELECT 2nd-In-File ASSIGN TO "Input2.csv"
+           SELECT 2nd-Input-File ASSIGN TO "Input2.csv"
            ORGANISATION LINE SEQUENTIAL
            ACCESS MODE SEQUENTIAL.
 
-           SELECT 3rd-In-File ASSIGN TO "Input3.csv"
+           SELECT 3rd-Input-File ASSIGN TO "Input3.csv"
            ORGANISATION LINE SEQUENTIAL
            ACCESS MODE SEQUENTIAL.
 
