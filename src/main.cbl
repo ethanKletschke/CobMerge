e4f23b7be42c912296be6394292ea41f8226e996
Ethan Kletschke <bbruhingson@gmail.com>
2026-02-17 10:55:38 +0200

    feat: Define input, merge, and output files in ENVIRONMENT division

--- src/main.cbl
+++ src/main.cbl
@@ -1,6 +1,27 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. CobMerge.
 
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT 1st-In-File ASSIGN TO "Input1.csv"
+           ORGANISATION LINE SEQUENTIAL
+           ACCESS MODE SEQUENTIAL.
+
+           SELECT 2nd-In-File ASSIGN TO "Input2.csv"
+           ORGANISATION LINE SEQUENTIAL
+           ACCESS MODE SEQUENTIAL.
+
+           SELECT 3rd-In-File ASSIGN TO "Input3.csv"
+           ORGANISATION LINE SEQUENTIAL
+           ACCESS MODE SEQUENTIAL.
+
+           SELECT Merge-File ASSIGN TO "Merge.csv".
+
+           SELECT Out-File ASSIGN TO "Output.csv
+           ORGANISATION LINE SEQUENTIAL
+           ACCESS MODE SEQUENTIAL.
+
        PROCEDURE DIVISION.
            DISPLAY "CobMerge v0.0.1".
            STOP RUN.
